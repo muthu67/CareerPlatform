54cbef4d755826799e65af3fbaea57c89ca74fa3
muthumaniwithu@gmail.com <muthumaniwithu@gmail.com>
2018-09-18 09:29:56 +0530

    2 pages

--- CareerPlatform/Controllers/HomeController.cs
+++ CareerPlatform/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using CareerPlatform.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +29,41 @@ namespace CareerPlatform.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult ValidateLogin()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult EmployeeLanding()
+        {
+            return View();
+        }
+        public ActionResult EmployerLanding()
+        {
+            return View();
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult CreateUser(string firstname,string lastname,string age,string email,string password,string telephone,string address)
+        {
+            var existing = Server.MapPath("/Database/datas/user.txt");
+            StringBuilder sb = new StringBuilder();
+            sb.Append(firstname + "|" + lastname + "|" + age + "|" + email + "|" + password + "|" + telephone + "|" + address);
+
+            if(System.IO.File.Exists(existing))
+                using (StreamWriter sw=new StreamWriter(existing,true))
+                {
+                    sw.WriteLine(sb.ToString());
+                }
+            
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ CareerPlatform/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CareerPlatform.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public long Telephone { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string Type { get; set; }
+    }
+}
